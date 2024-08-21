51953b9679fb31256b06b14cfa7343cd36e48a3b
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-21 12:11:37 -0300

    chore: project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from "./pages/home/Home"
+
+
+function App() {
+// código typescript - lógica
+
+  return (
+    // código tsx => html+css
+    <>
+     <Home/>
+    </>
+  )
+}
+
+export default App
