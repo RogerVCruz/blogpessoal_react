9a52cdb1e08447ec4f7f2071cd52feaed3e3a1ae
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-26 16:12:18 -0300

    feat: add auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import './App.css';
+import "./App.css";
 
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/home/Home';
-
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-   
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
-          <div className='min-h-[80vh]'>
+          <div className="min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Cadastro />} />
+              <Route path="/" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-  
+      </AuthProvider>
     </>
   );
 }
