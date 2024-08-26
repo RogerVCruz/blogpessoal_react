5b2745e1b2a08f9d510939184d524d870bb0bf63
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-26 10:22:33 -0300

    feat: add cadastro user

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Footer from "./components/footer/Footer"
-import Navbar from "./components/navbar/Navbar"
-import Home from "./pages/home/Home"
+import React from 'react';
+import './App.css';
+
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+
 
 function App() {
-  // Código TypeScript
-
   return (
-    // Código TSX => HTML + CSS
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
     </>
-  )
+  );
 }
-
-export default App
+export default App;
