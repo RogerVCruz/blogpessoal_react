9e2fbfbb532810f0cb6432d2761f7fbffee9cc2a
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-26 09:38:29 -0300

    feat: add router

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,22 +1,32 @@
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
- 
-  return (
-    <>
-        <div className="flex justify-center bg-indigo-900 text-white">
-          <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Blog pessoal Do Roger | Copyright: </p>
-            <p className='text-lg'>Acesse minhas redes sociais</p>
-            <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' />
-              <InstagramLogo size={48} weight='bold' />
-              <FacebookLogo size={48} weight='bold' />
+
+    let data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                        Blog Pessoal do Roger | Copyright: {data}
+                    </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+                        <a href="" target="_blank">
+                            <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                        <a href="" target="_blank">
+                        <InstagramLogo size={48} weight='bold' />
+                        </a>
+                        <a href="" target="_blank">
+                        <FacebookLogo size={48} weight='bold' />
+                        </a>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </>
-  )
+        </>
+    )
 }
 
 export default Footer
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,26 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
- 
     return (
-      <>
-       <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-            <div className="container flex justify-between text-lg">
-              <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
-  
-              <div className='flex gap-4'>
-                <div className='hover:underline'>Postagens</div>
-                <div className='hover:underline'>Temas</div>
-                <div className='hover:underline'>Cadastrar tema</div>
-                <div className='hover:underline'>Perfil</div>
-                <div className='hover:underline'>Sair</div>
-              </div>
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                
+                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        Sair
+                    </div>
+                </div>
             </div>
-          </div>
-      </>
+        </>
     )
-  }
-  
-  export default Navbar
+}
+
+export default Navbar
