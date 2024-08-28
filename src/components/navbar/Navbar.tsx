243eeada593d02224d14a596bf6c22a6e1d58cd0
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-28 09:26:08 -0300

    refac: add temas to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,31 +1,35 @@
-import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 function Navbar() {
-  const navigate = useNavigate();
+  let navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext);
+  const { usuario, handleLogout } = useContext(AuthContext);
 
   function logout() {
     handleLogout();
-    alert("O usuário foi desconectado com sucesso!");
-    navigate("/");
+    alert("Usuário deslogado com sucesso");
+    navigate("/login");
   }
+
+  let navbarComponent;
 
   return (
     <>
-      <div
-        className="w-full flex justify-center py-4
-            			   bg-indigo-900 text-white"
-      >
+      <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
         <div className="container flex justify-between text-lg">
-          <Link to="/home" className="text-2xl font-bold">
+          <Link to="/home" className="text-2xl font-bold uppercase">
             Blog Pessoal
           </Link>
 
           <div className="flex gap-4">
-            Postagens Temas Cadastrar tema Perfil
+            <div className="hover:underline">Postagens</div>
+            <Link to="/temas" className="hover:underline">
+              Temas
+            </Link>
+            <div className="hover:underline">Cadastrar tema</div>
+            <div className="hover:underline">Perfil</div>
             <Link to="" onClick={logout} className="hover:underline">
               Sair
             </Link>
