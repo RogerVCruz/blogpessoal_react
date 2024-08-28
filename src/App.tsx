3a4ddbc9694eee4e430bcfcfa813fc072383fbfa
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-28 09:16:52 -0300

    feat: lista and card from temas

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Cadastro from "./pages/cadastro/Cadastro";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/temas/listaTemas/ListaTemas";
 
 function App() {
   return (
@@ -17,9 +18,10 @@ function App() {
           <div className="min-h-[80vh]">
             <Routes>
               <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
             </Routes>
           </div>
           <Footer />
